6bef6051636b67c6b88a6e86f0e1c7b121b03360
Ben Bader <ben@bendb.com>
2017-03-19 13:39:00 -0700

    Add gradient to MemoryWidget

--- SysInfo/memorywidget.cpp
+++ SysInfo/memorywidget.cpp
@@ -8,13 +8,26 @@ using namespace QtCharts;
 
 const int CHART_X_RANGE_COUNT = 50;
 const int CHART_X_RANGE_MAX = CHART_X_RANGE_COUNT - 1;
+const int COLOR_DARK_BLUE = 0x209FDF;
+const int COLOR_LIGHT_BLUE = 0xBFDFEF;
+const int PEN_WIDTH = 3;
 
 MemoryWidget::MemoryWidget(QWidget *parent) :
     SysInfoWidget(parent),
     series_(new QLineSeries(this)),
     posX_(0)
 {
+    QPen pen(COLOR_DARK_BLUE);
+    pen.setWidth(PEN_WIDTH);
+
+    QLinearGradient gradient(QPointF(0, 0), QPointF(0, 1));
+    gradient.setColorAt(1.0, COLOR_DARK_BLUE);
+    gradient.setColorAt(0.0, COLOR_LIGHT_BLUE);
+    gradient.setCoordinateMode(QGradient::ObjectBoundingMode);
+
     QAreaSeries *areaSeries = new QAreaSeries(series_);
+    areaSeries->setPen(pen);
+    areaSeries->setBrush(gradient);
 
     QChart *chart = chartView().chart();
     chart->addSeries(areaSeries);
